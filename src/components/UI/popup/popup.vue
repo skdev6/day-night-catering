a133782217a26e6262920fe189baf58edf72264b
skdev6 <sukhan471@gmail.com>
2025-11-04 21:36:01 +0600

    update

--- src/components/UI/popup/popup.vue
+++ src/components/UI/popup/popup.vue
@@ -39,7 +39,7 @@ export default {
   left: 0;
   right: 0;
   bottom: 0;
-  background: rgba(0, 0, 0, 0.6);
+  background: rgba(0, 0, 0, 0.4);
   display: flex;
   align-items: center;
   justify-content: center;
